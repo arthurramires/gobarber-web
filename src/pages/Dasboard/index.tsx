1c74b8639d625113b822b8dda56591b03267288a
arthurramires <arthurramires1802@outlook.com>
2020-05-28 16:11:09 -0400

    Estilizando próximos agendamentos

--- src/pages/Dasboard/index.tsx
+++ src/pages/Dasboard/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+    Container,
+    Header,
+    HeaderContent,
+    Profile,
+    Content,
+    Schedule,
+    Calendar,
+    NextAppointment,
+} from './styles';
 import { useAuth } from '../../hooks/Auth';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const logo = require('../../assets/logo.svg');
@@ -26,6 +35,33 @@ const Dashboard: React.FC = () => {
                     </button>
                 </HeaderContent>
             </Header>
+
+            <Content>
+                <Schedule>
+                    <h1>Horários Agendados</h1>
+                    <p>
+                        <span>Hoje</span>
+                        <span>Dia 06</span>
+                        <span>Segunda-feira</span>
+                    </p>
+
+                    <NextAppointment>
+                        <strong>Atendimento a seguir</strong>
+                        <div>
+                            <img
+                                src="https://avatars3.githubusercontent.com/u/52502727?s=460&u=20d8fd7c0856631cbd5ba7c2876bb726a49cce02&v=4"
+                                alt="Arthur Ramires"
+                            />
+                            <strong>Arthur Ramires</strong>
+                            <span>
+                                <FiClock />
+                                08:00
+                            </span>
+                        </div>
+                    </NextAppointment>
+                </Schedule>
+                <Calendar></Calendar>
+            </Content>
         </Container>
     );
 };
